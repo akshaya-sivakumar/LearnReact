f1b7bde6dc0907b6e8b2d489d7987b2bfc476927
akshaya.sivakumar <akshaya.s@marketsimplified.com>
2023-01-30 11:29:57 +0530

    customize styles

--- lib/pages/watchlist.tsx
+++ lib/pages/watchlist.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { LayoutAnimation, Text, View } from 'react-native';
+import { ActivityIndicator, Alert, Dimensions, LayoutAnimation, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ExpandableComponent from '../widgets/expandable';
 
@@ -32,414 +32,8 @@ interface Data {
 
 const Watchlist: React.FC<Data> = () => {
 
-    const watchlist: Data[] = [
-        {
-            "baseSym": "KNRCON",
-            "companyName": "KNR CONSTRU LTD.",
-            "dispSym": "KNRCON",
-            "excToken": "532942",
-            "haircut": "70",
-            "isin": "INE634I01029",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_KNRCON_A_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "532942_BSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "SINTEXPLAST",
-            "companyName": "SINTEX PLASTICS TECH LTD",
-            "dispSym": "SINTEXPLAST",
-            "excToken": "540653",
-            "haircut": "0",
-            "isin": "INE501W01021",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_SINTEXPLAST_B_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "540653_BSE",
-                "strike": "0.00",
-                "tickSize": "0.01"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "INTELSOFT",
-            "companyName": "INTEGRA TELECOMMUNICATION & SO",
-            "dispSym": "INTELSOFT",
-            "excToken": "536868",
-            "haircut": "0",
-            "isin": "INE256F01019",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_INTELSOFT_XT_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "536868_BSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": true,
-
-        },
-        {
-            "baseSym": "GICHSGFIN",
-            "companyName": "GIC HOUSING FINANCE LTD",
-            "dispSym": "GICHSGFIN",
-            "excToken": "1139",
-            "haircut": "50",
-            "isin": "INE289B01019",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "NSE",
-                "expiry": "",
-                "id": "STK_GICHSGFIN_EQ_NSE",
-                "instrument": "STK",
-                "lotSize": "1",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "1139_NSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "IBULHSGFIN",
-            "companyName": "INDIABULLS HSG FIN LTD",
-            "dispSym": "IBULHSGFIN",
-            "excToken": "30125",
-            "haircut": "50",
-            "isin": "INE148I01020",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "NSE",
-                "expiry": "",
-                "id": "STK_IBULHSGFIN_EQ_NSE",
-                "instrument": "STK",
-                "lotSize": "1",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "30125_NSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "BERGEPAINT",
-            "companyName": "BERGER PAINTS (I) LTD",
-            "dispSym": "BERGEPAINT",
-            "excToken": "509480",
-            "haircut": "80",
-            "isin": "INE463A01038",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_BERGEPAINT_A_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "509480_BSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "BAJFINANCE",
-            "companyName": "BAJAJ FINANCE LIMITED",
-            "dispSym": "BAJFINANCE",
-            "excToken": "317",
-            "haircut": "80",
-            "isin": "INE296A01024",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "NSE",
-                "expiry": "",
-                "id": "STK_BAJFINANCE_EQ_NSE",
-                "instrument": "STK",
-                "lotSize": "1",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "317_NSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "PIIND",
-            "companyName": "PI INDUSTRIES LTD",
-            "dispSym": "PIIND",
-            "excToken": "523642",
-            "haircut": "80",
-            "isin": "INE603J01030",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_PIIND_A_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "523642_BSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "TCS",
-            "companyName": "TATA CONSULTANCY SERV LT",
-            "dispSym": "TCS",
-            "excToken": "532540",
-            "haircut": "80",
-            "isin": "INE467B01029",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_TCS_A_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "532540_BSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "WIPRO",
-            "companyName": "WIPRO LTD",
-            "dispSym": "WIPRO",
-            "excToken": "507685",
-            "haircut": "80",
-            "isin": "INE075A01022",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_WIPRO_A_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "507685_BSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "SBICARD",
-            "companyName": "SBI CARDS & PAY SER LTD",
-            "dispSym": "SBICARD",
-            "excToken": "17971",
-            "haircut": "80",
-            "isin": "INE018E01016",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "NSE",
-                "expiry": "",
-                "id": "STK_SBICARD_EQ_NSE",
-                "instrument": "STK",
-                "lotSize": "1",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "17971_NSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "LEMONTREE",
-            "companyName": "LEMON TREE HOTELS LTD",
-            "dispSym": "LEMONTREE",
-            "excToken": "541233",
-            "haircut": "70",
-            "isin": "INE970X01018",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_LEMONTREE_A_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "541233_BSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "NATURAL",
-            "companyName": "Natural Biocon (India) Limited",
-            "dispSym": "NATURAL",
-            "excToken": "543207",
-            "haircut": "0",
-            "isin": "INE06MM01016",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_NATURAL_X_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "543207_BSE",
-                "strike": "0.00",
-                "tickSize": "0.01"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "TATAMOTORS",
-            "companyName": "TATA MOTORS LIMITED",
-            "dispSym": "TATAMOTORS",
-            "excToken": "3456",
-            "haircut": "70",
-            "isin": "INE155A01022",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "NSE",
-                "expiry": "",
-                "id": "STK_TATAMOTORS_EQ_NSE",
-                "instrument": "STK",
-                "lotSize": "1",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "3456_NSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "SBIN",
-            "companyName": "STATE BANK OF INDIA",
-            "dispSym": "SBIN",
-            "excToken": "3045",
-            "haircut": "80",
-            "isin": "INE062A01020",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "NSE",
-                "expiry": "",
-                "id": "STK_SBIN_EQ_NSE",
-                "instrument": "STK",
-                "lotSize": "1",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "3045_NSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        },
-        {
-            "baseSym": "SBIN",
-            "companyName": "STATE BANK OF INDIA",
-            "dispSym": "SBIN",
-            "excToken": "500112",
-            "haircut": "80",
-            "isin": "INE062A01020",
-            "mCap": "",
-            "sector": "EQ",
-            "sym": {
-                "asset": "Equity",
-                "exc": "BSE",
-                "expiry": "",
-                "id": "STK_SBIN_A_BSE",
-                "instrument": "STK",
-                "lotSize": "0",
-                "multiplier": "1",
-                "optionType": "",
-                "streamSym": "500112_BSE",
-                "strike": "0.00",
-                "tickSize": "0.05"
-            },
-            "tt_eligibility": false,
-
-        }
-    ];
-
-
-
-
-
     const [expanded, setExpanded] = useState<string[]>([]);
+    const [watchlist, setWatchlist] = useState<Data[]>([]);
 
     function handleToggleComplete(id: any) {
         if (expanded.includes(id)) {
@@ -448,60 +42,70 @@ const Watchlist: React.FC<Data> = () => {
         } else {
             setExpanded([...expanded, id]);
         }
-
-
-
     }
+    React.useEffect(() => {
 
+        handleSubmit()
+    }, []);
 
+    const handleSubmit = async () => {
+        try {
+            const response = await fetch('https://run.mocky.io/v3/ec1a0f0c-3739-4914-b5c0-2140e5165fe5', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ "request": { "appID": "f79f65f1b98e116f40633dbb46fd5e21", "data": { "category": "TOPGAINER", "sessionID": "guest", "usrID": "guest" }, "formFactor": "M", "futures": "0", "response_format": "json" } })
+            });
 
-
-
-
-
-
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            const data = await response.json();
+            setWatchlist(data.response.data.symbols as Data[])
+        } catch (error: any) {
+            Alert.prompt('Error', error.message);
+        }
+    };
     return (
-
-
-
-
         <SafeAreaView style={{ flex: 1 }} >
-            <View style={{ alignContent: "center", alignItems: "center", paddingVertical: 20 }}>
-                <Text style={{ fontWeight: "bold", fontSize: 18, color: "black" }}>
-
+            <View style={styles.watchlistHeaderContainer}>
+                <Text style={styles.watchlistHeader}>
                     Msil Watchlist
-
                 </Text>
             </View>
-
-
-            {watchlist.map((item, index) => (
-                <View key={index} style={{ paddingHorizontal: 20 }}>
-                    <ExpandableComponent
-
-                        expandedList={expanded}
-                        onClickFunction={() => {
-                            console.warn(index)
-                            LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-                            handleToggleComplete(item.excToken)
-
-                        }}
-                        item={watchlist[index]}
-                    />
-                </View>
-            )
-
-
-
-
-            )}
-
-
-
-
-
+            <View>
+                {watchlist.length > 0 ? (<View>
+                    {watchlist?.map((item, index) => (
+                        <View key={index} style={{ paddingHorizontal: 20 }}>
+                            <ExpandableComponent
+                                expandedList={expanded}
+                                onClickFunction={() => {
+                                    console.warn(index)
+                                    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+                                    handleToggleComplete(item.excToken)
+                                }}
+                                item={watchlist[index]}
+                            />
+                        </View>
+                    )
+                    )}
+                </View>) : (
+                    <ActivityIndicator style={styles.container} size="large" color="#0000ff" />
+                )}
+            </View>
         </SafeAreaView>
     )
 }
 
 export default Watchlist
+
+const styles = StyleSheet.create({
+    watchlistHeaderContainer: { alignContent: "center", alignItems: "center", paddingVertical: 20 },
+    watchlistHeader: { fontWeight: "bold", fontSize: 18, color: "black" },
+    container: {
+        height: Dimensions.get('window').height * 0.9,
+        justifyContent: 'center', alignItems: 'center'
+    },
+
+})
--- lib/widgets/expandable.tsx
+++ lib/widgets/expandable.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { Dimensions, Text, TouchableOpacity, View } from "react-native";
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-/* const ExpandableComponent = ({ item, onClickFunction }) => */
+
 
 export type ExpandableComponentProps = {
     item: any;
@@ -13,9 +13,7 @@ const ExpandableComponent: React.FC<ExpandableComponentProps> = ({
     item, expandedList,
     onClickFunction,
 }) => {
-    //Custom Component for the Expandable List
     const [layoutHeight, setLayoutHeight] = useState(0);
-
     useEffect(() => {
         if (expandedList.includes(item.excToken)) {
             setLayoutHeight(80);
@@ -23,32 +21,27 @@ const ExpandableComponent: React.FC<ExpandableComponentProps> = ({
             setLayoutHeight(0);
         }
     }, [expandedList]);
-
     return (
 
-        <View key={item.excToken} style={{
+        <View key={item.excToken} >
 
-
-
-        }}>
-            {/*Header of the Expandable List Item*/}
             < TouchableOpacity
                 activeOpacity={0.8}
                 onPress={onClickFunction} style={{ backgroundColor: item.expanded ? 'rgba(52, 52, 52, 0.2)' : 'rgba(52, 52, 52, 0.0)', }}
-            ><View style={{ flexDirection: "row", justifyContent: "space-between", paddingVertical: 5 }}>
-                    <View style={{ flexDirection: "column", width: Dimensions.get("window").width * 0.55 }}>
-                        <Text style={{ fontWeight: "bold", color: "black" }}> {item.baseSym}</Text>
-                        <Text style={{ paddingTop: 5 }}>{item.companyName} </Text>
+            ><View style={styles.expandedHeaderview}>
+                    <View style={styles.headeFirstcolumn}>
+                        <Text style={styles.basesymstyle}> {item.baseSym}</Text>
+                        <Text style={styles.cmpyNamestyle}>{item.companyName} </Text>
                     </View>
-                    <View style={{ flexDirection: "column", }}>
+                    <View style={styles.headerSecondColumn}>
 
-                        <Text style={{ color: "#ff1493" }}>BSE </Text>
+                        <Text style={styles.headerSecondColumntext}>BSE </Text>
                     </View>
-                    <View style={{ flexDirection: "column", width: Dimensions.get("window").width * 0.14, alignSelf: 'flex-start', justifyContent: "flex-start", alignItems: "flex-start", alignContent: "flex-start" }}>
-                        <Text style={{ color: "red", textAlign: "right" }}> {item.excToken}</Text>
-                        <View style={{ flexDirection: "row", paddingTop: 5, justifyContent: "space-evenly", width: Dimensions.get("window").width * 0.2, }}>
-                            <Text style={{ color: "orange" }}>{item.haircut} </Text>
-                            <Text style={{ color: "green", }}>{item.haircut} </Text>
+                    <View style={styles.headerthirdColumn}>
+                        <Text style={styles.exctokenStyle}> {item.excToken}</Text>
+                        <View style={styles.haircutrow}>
+                            <Text style={styles.haircutStyle}>{item.haircut} </Text>
+                            <Text style={styles.haircutStyle2}>{item.haircut} </Text>
                         </View>
                     </View>
                 </View>
@@ -69,17 +62,13 @@ const ExpandableComponent: React.FC<ExpandableComponentProps> = ({
                 }}>
                 {/*Content under the header of the Expandable List Item*/}
 
-                <TouchableOpacity style={{ paddingVertical: 5, backgroundColor: 'rgba(52, 52, 52, 0.2)', }}
-
-                    onPress={
-                        () => { }
-                    }>
-                    <View style={{ flexDirection: "row", justifyContent: "space-evenly", paddingVertical: 10 }}>
-                        <TouchableOpacity onPress={() => { }} style={{ backgroundColor: "green", borderRadius: 10, alignItems: 'center', width: 100, paddingVertical: 5, }}>
-                            <Text style={{ color: "white", fontSize: 18, fontWeight: 'bold' }}>BUYY</Text>
+                <TouchableOpacity style={styles.expandedTouch}>
+                    <View style={styles.expandedView}>
+                        <TouchableOpacity onPress={() => { }} style={styles.buyButton}>
+                            <Text style={styles.buyButtontext}>BUYY</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { }} style={{ backgroundColor: "red", borderRadius: 10, alignItems: 'center', width: 100, paddingVertical: 5, }}>
-                            <Text style={{ color: "white", fontSize: 18, fontWeight: 'bold' }}>SELL</Text>
+                        <TouchableOpacity onPress={() => { }} style={styles.sellButton}>
+                            <Text style={styles.sellButtontext}>SELL</Text>
                         </TouchableOpacity>
                     </View>
                     <View />
@@ -93,3 +82,27 @@ const ExpandableComponent: React.FC<ExpandableComponentProps> = ({
 };
 
 export default ExpandableComponent
+
+const styles = StyleSheet.create({
+
+    expandedHeaderview: { flexDirection: "row", justifyContent: "space-between", paddingVertical: 5 },
+    headeFirstcolumn: { flexDirection: "column", width: Dimensions.get("window").width * 0.55 },
+    basesymstyle: { fontWeight: "bold", color: "black" },
+    cmpyNamestyle: { paddingTop: 5 },
+    headerSecondColumn: { flexDirection: "column", },
+    headerSecondColumntext: { color: "#ff1493" },
+    headerthirdColumn: { flexDirection: "column", width: Dimensions.get("window").width * 0.14, alignSelf: 'flex-start', justifyContent: "flex-start", alignItems: "flex-start", alignContent: "flex-start" },
+    exctokenStyle: { color: "red", textAlign: "right" },
+    haircutrow: { flexDirection: "row", paddingTop: 5, justifyContent: "space-evenly", width: Dimensions.get("window").width * 0.2, },
+    haircutStyle: { color: "orange" },
+
+    haircutStyle2: { color: "green", },
+
+    expandedTouch: { paddingVertical: 5, backgroundColor: 'rgba(52, 52, 52, 0.2)', },
+    expandedView: { flexDirection: "row", justifyContent: "space-evenly", paddingVertical: 10 },
+    buyButton: { backgroundColor: "green", borderRadius: 10, alignItems: 'center', width: 100, paddingVertical: 5, },
+    buyButtontext: { color: "white", fontSize: 18, fontWeight: 'bold' },
+    sellButton: { backgroundColor: "red", borderRadius: 10, alignItems: 'center', width: 100, paddingVertical: 5, },
+    sellButtontext: { color: "white", fontSize: 18, fontWeight: 'bold' }
+
+})
